9c907d6572bb53dd93bdb8914cf4d7dfe25ebf5b
Rohit Jakhar <rohitjakhar940@gmail.com>
2022-03-13 19:50:42 +0530

    Create Seleton of Movie List UI.

---
+++ app/src/test/java/com/rohitjakhar/procreatortask/utils/MainCoroutineScopeRule.kt
@@ -0,0 +1,30 @@
+package com.rohitjakhar.procreatortask.utils
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.*
+import org.junit.rules.TestWatcher
+import org.junit.runner.Description
+
+@ExperimentalCoroutinesApi
+class MainCoroutineScopeRule(val dispatcher: TestDispatcher = StandardTestDispatcher()) :
+    TestWatcher(),
+    TestCoroutineScope by TestCoroutineScope(dispatcher) {
+    override fun starting(description: Description?) {
+        super.starting(description)
+        // If your codebase allows the injection of other dispatchers like
+        // Dispatchers.Default and Dispatchers.IO, consider injecting all of them here
+        // and renaming this class to `CoroutineScopeRule`
+        //
+        // All injected dispatchers in a test should point to a single instance of
+        // TestCoroutineDispatcher.
+        Dispatchers.setMain(dispatcher)
+    }
+
+    override fun finished(description: Description?) {
+        super.finished(description)
+        advanceUntilIdle()
+        cleanupTestCoroutines()
+        Dispatchers.resetMain()
+    }
+}
