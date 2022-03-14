c56fc30e38d7e22423e6130d84f55575d2573442
Rohit Jakhar <rohitjakhar940@gmail.com>
2022-03-14 13:56:53 +0530

    Implemented Movie Details UI

--- app/src/main/java/com/rohitjakhar/procreatortask/ui/movie_details/MovieDetailsFragment.kt
+++ app/src/main/java/com/rohitjakhar/procreatortask/ui/movie_details/MovieDetailsFragment.kt
@@ -1,6 +1,7 @@
 package com.rohitjakhar.procreatortask.ui.movie_details
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,6 +9,9 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.navArgs
+import coil.load
+import coil.transform.RoundedCornersTransformation
+import com.rohitjakhar.procreatortask.data.model.MovieDetailsModel
 import com.rohitjakhar.procreatortask.databinding.FragmentMovieDetailsBinding
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collectLatest
@@ -42,13 +46,30 @@ class MovieDetailsFragment : Fragment() {
                     // TODO: Show Loading Screen
                 } else {
                     if (it.movieDetails != null) {
-                        // TODO:  Show Data
+                        updateUI(it.movieDetails)
                     } else {
                         // TODO: Show Error Message
                     }
                 }
             }
         }
+    }
+
+    private fun updateUI(movieDetails: MovieDetailsModel) = binding.apply {
+        ivMovieCover.load(movieDetails.movieImage)
+        ivMoviePhoto.load(movieDetails.movieImage) {
+            transformations(RoundedCornersTransformation(12F))
+        }
+        tvMovieTitle.text = movieDetails.movieName
+        tvDirectorName.text = movieDetails.movieDirector
+        tvWritterName.text = movieDetails.movieWriter
+        tvGenreName.text = movieDetails.movieGenre
+        tvPhName.text = movieDetails.moviePH
+        ratingMovie.rating = movieDetails.movieRating.toFloat() / 2
+        tvMovieRating.text = "${movieDetails.movieRating}/10"
+        tvMovieDuration.text = "${movieDetails.movieDuration} Min"
+        tvMovieOverview.text = movieDetails.movieDescription
+        Log.d("test","genre: ${movieDetails.movieGenre}")
     }
 
     override fun onDestroy() {
